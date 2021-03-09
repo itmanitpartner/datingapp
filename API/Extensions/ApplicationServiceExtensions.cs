ede2697f2e512f368ae4363199f6e7e56e018ada
itpartneritalia <sviluppo.fullcrm.01@itpartneritalia.com>
2021-03-09 17:53:32 +0100

    End of section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+using API.Data;
+using API.interfaces;
+using API.services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            // interfaccia per la gestione dei tokens
+            services.AddScoped<ITokenService, TokenService>();
+
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+
+        }
+    }
+}
